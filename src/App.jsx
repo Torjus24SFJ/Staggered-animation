84cdbb5696f3e756941bce7a6b5536545a24f8ba
Torjus <Torjus24sfj@gmail.com>
2025-11-11 09:32:34 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { useRef } from 'react'
+import { useIntersectionObserver } from '@gsap/react'
+import gsap from 'gsap'
+import { useGSAP } from 'gsap/react'
+
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
